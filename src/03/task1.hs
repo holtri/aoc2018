12ec35e3f176c97f00d3088d87ce7ba71faeb6d1
Holger <holger.trittenbach@gmail.com>
2018-12-09 16:24:47 +0100

    First try Day 03.

--- src/03/task1.hs
+++ src/03/task1.hs
@@ -1,13 +1,34 @@
 import Data.Char
 import Data.List.Split
 
+n = 1000
+
 main :: IO ()
 main = do
-   input <- readFile "03/input"
-   print "eof"
+   input <- readFile "src/03/input"
+   let parsedInput = map parse $ lines input
+       output = intersectCuts $ map getcut $ take 20 parsedInput
+   print output
 
+parse =
+   let splitbynondigit = splitWhen (not . isDigit)
+       filterempty = filter (not . null)
+   in map (read::String->Integer) . filterempty . splitbynondigit
 
-   --   print (parseInput input)
-   --
-   -- parseInput =
-   --   map (read::String->Int) $ filter (not . null) $ splitWhen (not . isDigit)
+inbox :: (Num t, Num t1, Ord t1) => (t1, t1) -> [t1] -> t
+inbox (x,y) [id,from_left,from_top,width,height] =
+   if in_horizontal && in_vertical then 1 else 0
+   where in_horizontal = x  > from_left && x <= from_left + width
+         in_vertical = y > from_top && y <= from_top + height
+
+getcut claim = [(x,y) `inbox` claim | x <- [1..n], y <- [1..n]]
+
+intersectCuts :: (Foldable t, Integral c) => t [c] -> Int
+intersectCuts = length . filter (> 1) . foldl1 (zipWith (+))
+
+a = [1,1,3,4,4]
+b = [2,3,1,4,4]
+c = [3,5,5,2,3]
+
+-- input <- readFile "src/03/input"
+-- x = map parseInput $lines input
