561ad07d50004a8711cd617726721875b09c3010
Holger <holger.trittenbach@gmail.com>
2018-12-09 22:43:04 +0100

    Day 03 task2.

---
+++ src/03/task2.hs
@@ -0,0 +1,31 @@
+import Data.Char
+import Data.List.Split
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+   input <- readFile "src/03/input"
+   let parsedInput = map parse $ lines input
+       cuts = M.unionsWith (++) $ map getcut parsedInput
+       output_t1 = M.size $ M.filter (>1) $ M.map length cuts
+
+       single_cuts = M.filter (\ v -> length v == 1) cuts
+       cut_keys = map (M.keys . getcut) parsedInput
+       filtered_single_cut = map (containsAll single_cuts) cut_keys
+       output_t2 = [y | (x, y:ys) <- zip filtered_single_cut parsedInput, x]
+   print output_t1
+   print output_t2
+
+containsAll cuts = all (`M.member` cuts)
+
+parse = map (read::String->Integer) . filter (not . null) . splitWhen (not . isDigit)
+
+getcut :: [Integer] -> M.Map (Integer, Integer) [Integer]
+getcut [id,l_space,t_space,width,height] = M.fromList [((x,y), [id]) | x <- x_span, y <- y_span]
+   where x_span = [l_space+1 .. l_space + width]
+         y_span = [t_space + 1 .. t_space + height]
+
+-- a = [1,1,3,4,4]::[Integer]
+-- b = [2,3,1,4,4]::[Integer]
+-- c = [3,5,5,2,3]::[Integer]
+-- x = [a,b,c]
