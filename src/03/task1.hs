c675513e137fe1e779e795e2504502dd151b77e4
Holger <holger.trittenbach@gmail.com>
2018-12-09 18:01:03 +0100

    Update Day 03.

--- src/03/task1.hs
+++ src/03/task1.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Data.List.Split
+import qualified Data.Map as M
 
 n = 1000
 
@@ -7,7 +8,7 @@ main :: IO ()
 main = do
    input <- readFile "src/03/input"
    let parsedInput = map parse $ lines input
-       output = intersectCuts $ map getcut $ take 20 parsedInput
+       output = countIntersects $ parsedInput
    print output
 
 parse =
@@ -15,20 +16,22 @@ parse =
        filterempty = filter (not . null)
    in map (read::String->Integer) . filterempty . splitbynondigit
 
-inbox :: (Num t, Num t1, Ord t1) => (t1, t1) -> [t1] -> t
+inbox :: (Num t1, Ord t1) => (t1, t1) -> [t1] -> Bool
 inbox (x,y) [id,from_left,from_top,width,height] =
-   if in_horizontal && in_vertical then 1 else 0
+   in_horizontal && in_vertical
    where in_horizontal = x  > from_left && x <= from_left + width
          in_vertical = y > from_top && y <= from_top + height
 
-getcut claim = [(x,y) `inbox` claim | x <- [1..n], y <- [1..n]]
-
-intersectCuts :: (Foldable t, Integral c) => t [c] -> Int
-intersectCuts = length . filter (> 1) . foldl1 (zipWith (+))
+getcut :: Num a => [Integer] -> M.Map (Integer, Integer) a
+getcut claim = M.fromList [((x,y), 1) | x <- [1..n], y <- [1..n], (x,y) `inbox` claim]
 
 a = [1,1,3,4,4]
 b = [2,3,1,4,4]
 c = [3,5,5,2,3]
-
+x = [a,b,c]
 -- input <- readFile "src/03/input"
 -- x = map parseInput $lines input
+
+countIntersects =
+   M.size . M.filter (>1) . intersectCuts
+   where intersectCuts = foldl (M.unionWith (+)) M.empty . map getcut
