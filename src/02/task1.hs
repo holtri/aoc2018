cffe9071288ad2b0e468f75681fafcf74050d6e5
Holger <holger.trittenbach@gmail.com>
2018-12-22 23:21:36 +0100

    Finish Day 02 part 2.

--- src/02/task1.hs
+++ src/02/task1.hs
@@ -1,18 +1,25 @@
 import System.IO
-import Data.Map
+import qualified Data.Map as M
 
 main = do
-  input <- readFile "02/input"
-  let parsedInput = parse input
-  let checksum = (countOccurences 2 parsedInput) * (countOccurences 3 parsedInput)
-  print checksum
+  input <- readFile "src/02/input"
+  let parsedInput = lines input
+      checksum = countOccurences 2 parsedInput * countOccurences 3 parsedInput
+  print ("Task 1 : " ++ show checksum)
+  print ("Task 2 : " ++ show (findId $ lines input))
 
-parse input = lines input
-
-charCount s = toList $ fromListWith (+) [(c,1) | c <- s]
+charCount s = M.toList $ M.fromListWith (+) [(c,1) | c <- s]
 
 rev cc = [(b,a) | (a,b) <- cc]
 
-ismember x s = member x (fromList (rev $ charCount s))
+contains digit_count s = M.member digit_count (M.fromList (rev $ charCount s))
 
-countOccurences d input = sum [1 | x <- input, ismember d x]
+countOccurences digit_count input = sum [1 | x <- input, contains digit_count x]
+
+stringOverlap s1 s2 = [c1 | (c1, c2) <- zip s1 s2, c1 == c2]
+
+findId (x:xs) =
+    case overlap of
+        [] -> findId xs
+        (y:ys) -> y
+    where overlap = dropWhile (\i -> length i /= length x - 1) $ map (stringOverlap x) xs
