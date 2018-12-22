80feb3d2f72d5cb9fbecab07a7d34b810c151c03
Holger <holger.trittenbach@gmail.com>
2018-12-22 22:27:07 +0100

    Add Day 4.

--- src/04/task01.hs
+++ src/04/task01.hs
@@ -1,21 +1,42 @@
-import Data.Time
-import Data.List (sortBy)
-import Data.Function (on)
+import Data.List (sortBy, sort)
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
    input <- readFile "src/04/input"
-   let tmp = sortBy (compare `on` fst) $ map parselog $ lines input
-   print (map fst $ take 3 tmp)
 
-parsetime :: String -> UTCTime
-parsetime  = parseTimeOrError True defaultTimeLocale "[%Y-%m-%d %H:%M]"
+   let parsed_input = map parse $ sort $ lines input
+   let processed_log = sleeptimes parsed_input []
+   let split_minutes = [(worker_id, minute) | (worker_id, minutes) <- processed_log, minute <- minutes]
 
-parselog x = (parsetime $ fst logmsg, snd logmsg)
-  where logmsg = splitAt 19 x
+   let most_sleeping_id = fst . findMaxByValue $ M.toList $ M.fromListWith (+) $ map (\x -> (fst x, 1)) split_minutes
+   let minute_count = M.toList $ M.fromListWith (+) [(minute, 1) | (worker_id, minute) <- split_minutes, worker_id==most_sleeping_id]
 
+   let wid = read most_sleeping_id :: Integer
+   let top_min = fst $ findMaxByValue minute_count
+   print ("Task 1: " ++ show (wid * top_min))
 
-a = "[1518-05-31 00:27] falls asleep"
-b = "[1518-04-15 00:54] wakes up"
-c = "[1518-04-06 00:42] falls asleep"
-x = [a,b,c]
+   let asleep_count = findMaxByValue $ M.toList $ M.fromListWith (+) $ map (\x -> (x, 1)) split_minutes
+   let wid = read $ fst . fst $ asleep_count :: Integer
+   let selected_min = snd . fst $ asleep_count
+   print ("Task 2:" ++ show (wid * selected_min))
+
+findMaxByValue :: (Ord b) => [(a,b)] -> (a, b)
+findMaxByValue = foldl1 (\x y -> if snd x > snd y then x else y)
+
+parse :: String -> (Integer, String)
+parse logentry =
+   let logparts = words logentry
+       minute =  read $ take 2 $ drop 3 $ logparts !! 1  :: Integer
+       action = logparts !! 3
+   in  (minute, action)
+
+-- [(0,"#10"),(27,"asleep"),(54,"up"),(42,"asleep")]
+-- [("10", [42..?]), ("10", [27..53]), ("10", [])]
+
+sleeptimes :: [(Integer, String)] -> [(String, [Integer])] -> [(String, [Integer])]
+sleeptimes ((_, '#':id):xs) [] = sleeptimes xs [(id, [])]
+sleeptimes ((_, '#':id):xs) acc = sleeptimes xs ((id, []):acc)
+sleeptimes ((_, "up"):xs) acc = sleeptimes xs acc
+sleeptimes ((x1, "asleep"):(x2,_):xs) acc = sleeptimes xs ((fst $ head acc, [x1..x2-1]):acc)
+sleeptimes _ acc = acc
