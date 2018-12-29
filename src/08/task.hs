b4949718a752bc2084ab9c5b0b97297ade82eb2c
Holger <holger.trittenbach@gmail.com>
2018-12-29 18:48:19 +0100

    Add Day 08 part 2.

--- src/08/task.hs
+++ src/08/task.hs
@@ -5,16 +5,16 @@ main :: IO ()
 main = do
    input <- readFile "src/08/input"
    let parsedInput =  map (read::String->Integer) $ words input
-   print ("Task 1: " ++ show (sumMetadata $ buildTree parsedInput))
-
-tmp = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+       tree = buildTree parsedInput
+   print ("Task 1: " ++ show (sumMetadata tree))
+   print ("Task 2: " ++ show (value tree))
 
 buildTree :: [Integer] -> Tree
 buildTree (nc:nm:xs) = root
    where (root, _ ) = parseNode [] nc nm xs
 
 parseNode :: [Tree] -> Integer -> Integer -> [Integer] -> (Tree, [Integer])
-parseNode childs 0 n_meta xs = (Tree{metadata=metadata, childs=childs}, remaining)
+parseNode childs 0 n_meta xs = (Tree{metadata=metadata, childs=reverse childs}, remaining)
    where (metadata, remaining) = splitAt (fromIntegral n_meta) xs
 parseNode childs n_childs n_meta (nc:nm:xs) =
    let (child, remaining) = parseNode [] nc nm xs
@@ -22,3 +22,9 @@ parseNode childs n_childs n_meta (nc:nm:xs) =
 
 sumMetadata :: Tree -> Integer
 sumMetadata tree = sum (metadata tree) + sum (map sumMetadata (childs tree))
+
+value :: Tree -> Integer
+value (Tree metadata []) = sum metadata
+value (Tree metadata childs) =
+   let validChilds = filter (\x -> x/=0 && x <= fromIntegral (length childs)) metadata
+   in sum $ map (\i -> value (childs !! fromIntegral (i-1))) validChilds
