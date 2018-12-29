99c2117ab6c84774fc10cd61469277e1929e523f
Holger <holger.trittenbach@gmail.com>
2018-12-29 18:13:32 +0100

    Refactor Day 08.

--- src/08/task.hs
+++ src/08/task.hs
@@ -1,20 +1,24 @@
 
+data Tree = Tree {metadata :: [Integer], childs :: [Tree]} deriving (Show)
+
 main :: IO ()
 main = do
    input <- readFile "src/08/input"
    let parsedInput =  map (read::String->Integer) $ words input
-   print ("Task 1: " ++ show (processTree parsedInput))
-
-processTree :: [Integer] -> Integer
-processTree (a:b:xs) = acc
-   where (acc, _ ) = process 0 a b xs
-
-process :: Integer -> Integer -> Integer -> [Integer] -> (Integer, [Integer])
-process acc _ _ [] = (acc, [])
-process acc 0 payload xs = (acc + sum (take p xs), drop p xs)
-   where p = fromInteger payload
-process acc childs payload (c:p:xs) =
-      let (acc2, remaining) = process acc c p xs
-      in process acc2 (childs - 1) payload remaining
+   print ("Task 1: " ++ show (sumMetadata $ buildTree parsedInput))
 
 tmp = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+
+buildTree :: [Integer] -> Tree
+buildTree (nc:nm:xs) = root
+   where (root, _ ) = parseNode [] nc nm xs
+
+parseNode :: [Tree] -> Integer -> Integer -> [Integer] -> (Tree, [Integer])
+parseNode childs 0 n_meta xs = (Tree{metadata=metadata, childs=childs}, remaining)
+   where (metadata, remaining) = splitAt (fromIntegral n_meta) xs
+parseNode childs n_childs n_meta (nc:nm:xs) =
+   let (child, remaining) = parseNode [] nc nm xs
+   in parseNode (child:childs) (n_childs - 1) n_meta remaining
+
+sumMetadata :: Tree -> Integer
+sumMetadata tree = sum (metadata tree) + sum (map sumMetadata (childs tree))
